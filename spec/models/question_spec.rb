366fea1acbe59be3aa92a1e77b1fafacdb9d2f74
Ad1n <shevtsovav@bk.ru>
2018-10-29 19:50:27 +0300

    Lesson 3. Corrections 2.

--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -4,14 +4,6 @@ RSpec.describe Question, type: :model do
   it { should validate_presence_of(:title) }
   it { should validate_presence_of(:body) }
 
-  it { should have_many(:answers) }
-
-  it 'destroys dependent answers' do
-    question = FactoryBot.build(:question)
-    answer = FactoryBot.build(:answer)
-    question.answers << answer
-
-    expect(question).to have_many(:answers).dependent(:destroy)
-  end
+  it { should have_many(:answers).dependent(:destroy) }
 
 end
