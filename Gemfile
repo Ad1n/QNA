e5c4e3b40d32173a633f7d1986fb7c80eadac05e
Ad1n <shevtsovav@bk.ru>
2018-11-25 16:28:28 +0300

    Attachment model

--- Gemfile
+++ Gemfile
@@ -44,6 +44,9 @@ gem 'bootsnap', '>= 1.1.0', require: false
 gem 'slim-rails'
 gem 'devise'
 
+#Attach files
+gem 'carrierwave'
+
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -1,10 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe Question, type: :model do
+  it { should have_many(:answers).dependent(:destroy) }
+  it { should have_many(:attachments) }
+
   it { should validate_presence_of(:title) }
   it { should validate_presence_of(:body) }
 
-  it { should have_many(:answers).dependent(:destroy) }
   it { should belong_to(:user) }
 
 end
